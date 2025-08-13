c114033c4bdb439a65b0c161b597cdc517cc7305
samii-15 <163318417+samii-15@users.noreply.github.com>
2025-08-13 15:49:53 +0530

    Update glorious_jaiks1(2).ino

--- glorious_jaiks1(2).ino
+++ glorious_jaiks1(2).ino
@@ -1,5 +1,3 @@
-// C++ code
-//
 void setup()
 {
   pinMode(2, OUTPUT);
@@ -12,11 +10,11 @@ void loop()
   int status=0;
   status=digitalRead(9);
   if(status==1){
-  digitalWrite(2, HIGH);
-  delay(200); 
+    digitalWrite(2, HIGH);
+    delay(200); 
   }
-  else{// Wait for 1000 millisecond(s)
-  digitalWrite(2, LOW);
-  delay(200); // Wait for 1000 millisecond(s)
-}
+  else{
+   digitalWrite(2, LOW);
+    delay(200);
+  }
 }
